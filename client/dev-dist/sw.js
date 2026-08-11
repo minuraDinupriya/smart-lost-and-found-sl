476348c390471c0b71f798dfec1ca6f851ad0053
User <user@example.com>
2026-08-11 20:55:16 +0530

    feat: simplify theme toggle and add system default option

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.244mh2sa1cs"
+    "revision": "0.fji3tf4b7lk"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
